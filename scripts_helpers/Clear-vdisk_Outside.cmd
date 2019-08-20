58f11ce4e292fdb3930c875b76d37e82dc73fe68
mitmih <38176228+mitmih@users.noreply.github.com>
2019-08-20 14:37:54 +0300

    Update Clear-vdisk_Outside.cmd

--- scripts_helpers/Clear-vdisk_Outside.cmd
+++ scripts_helpers/Clear-vdisk_Outside.cmd
@@ -2,13 +2,5 @@
 
 
 
-echo SDelete files
-echo https://download.sysinternals.com/files/SDelete.zip
-echo.
-echo Full ToolSet
-echo https://download.sysinternals.com/files/SysinternalsSuite.zip
-
-
-
 REM host, in PowerShell
     & 'C:\Program Files\Oracle\VirtualBox\VBoxManage.exe' modifyhd "d:\_vm\7\7.vdi" --compact
