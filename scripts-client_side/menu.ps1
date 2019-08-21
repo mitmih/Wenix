022219be83c43d61b03ee24c6cea5f5bd0d7ed48
mitmih <38176228+mitmih@users.noreply.github.com>
2019-08-21 12:22:17 +0300

    Update menu.ps1

--- scripts-client_side/menu.ps1
+++ scripts-client_side/menu.ps1
@@ -13,47 +13,19 @@ Please press specified key to select action:
 $MenuText
 
 
-$cycle = $true
-while ($cycle)
+$cycle = $true ; while ($cycle)
 {
     # $key = Read-Host -Prompt "press any key..."
     $key = [console]::ReadKey()
     
     switch ($key.key)
     {
-        'Enter'     { Write-Host "enter, операционная система будет переустановлена" ; break }
+        'Enter'     { Write-Host "Enter, installation process launched" ; break }
         
-        'Escape'    { Write-Host "EEscape, компьютер будет перезагружен" ; break }
+        'Escape'    { Write-Host "EEscape, PC will be restarted" ; break }
         
-        # 'm' { Clear-Host ; $MenuText }
+        'B'         { $cycle = $false ; break }
         
-        'B' { $cycle = $false ; break }
-        
-        # {$_ -notin 'Enter', 'Escape', 'm', 'b' } { continue }
-        
-        Default { Clear-Host ; $MenuText ; break }
+        Default     { Clear-Host ; $MenuText ; break }
     }
-    
-    
-    
-    # if ($key.key -eq 'Enter')
-    # {
-    #     Write-Host "enter, операционная система будет переустановлена"
-    # }
-    
-    # if ($key.key -eq 'Escape')
-    # {
-    #     Write-Host "  Escape, компьютер будет перезагружен"
-    # }
-    
-    # if ($key.key -eq 'm')
-    # {
-    #     Clear-Host
-    #     Write-Host $MenuText
-    # }
-    
-    # if ($key.key -eq 'b')
-    # {
-    #     break
-    # }
 }
