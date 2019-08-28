34740e7dfd9456bf2602aa17397dee46d0fc671e
mitmih <38176228+mitmih@users.noreply.github.com>
2019-08-29 01:37:38 +0300

    Update qwe.ps1

--- Wenix/qwe.ps1
+++ Wenix/qwe.ps1
@@ -1,24 +1,27 @@
 Import-Module -Force Wenix
 Get-Module -Name Wenix
 
-$file = Find-NetConfig
+# $file = Find-NetConfig
 
-$PEloc = Test-WimNet -md5 -ver 'PE' -name 'boot'    
-$OSloc = Test-WimNet -md5 -ver '10' -name 'install' 
+# $PEloc = Test-WimNet -md5 -ver 'PE' -name 'boot'    
+# $OSloc = Test-WimNet -md5 -ver '10' -name 'install' 
 
-$shares = @()
-if ($null -ne $file) { $shares += Read-NetConfig -file $file } # else { $shares = @() }
+# $shares = @()
+# if ($null -ne $file) { $shares += Read-NetConfig -file $file } # else { $shares = @() }
 
-# $ok = @{} ; $ver = '10' ; $name = 'install'
-if ($shares.Count -gt 0)
-{
-    # PE
-    $ver = 'PE' ; $name = 'boot'
-    $PEnet = Test-WimNet -SharesList $shares -ver $ver -name $name -md5
+# # $ok = @{} ; $ver = '10' ; $name = 'install'
+# if ($shares.Count -gt 0)
+# {
+#     # PE
+#     $ver = 'PE' ; $name = 'boot'
+#     $PEnet = Test-WimNet -SharesList $shares -ver $ver -name $name -md5
     
-    # OS
-    $ver = '10' ; $name = 'install'
-    $OSnet = Test-WimNet -SharesList $shares -ver $ver -name $name -md5 #:$false
-}
+#     # OS
+#     $ver = '10' ; $name = 'install'
+#     $OSnet = Test-WimNet -SharesList $shares -ver $ver -name $name -md5 #:$false
+# }
 
-$PEloc, $OSloc, $PEnet, $OSnet | ft *
+# $PEloc, $OSloc, $PEnet, $OSnet | ft *
+
+
+Use-Wenix
