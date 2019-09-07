663d65af5099749b31a5a2af5e8cb6d676cf7416
mitmih <38176228+mitmih@users.noreply.github.com>
2019-09-07 18:33:45 +0300

    Update Wenix-Debug.ps1

--- Wenix/Wenix-Debug.ps1
+++ Wenix/Wenix-Debug.ps1
@@ -27,12 +27,3 @@ Get-Module -Name Wenix
 
 
 # Use-Wenix -STOP
-
-$command = (Get-Command Add-Junctions).Definition
-$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
-$encodedCommand = [Convert]::ToBase64String($bytes)
-
-$AutoRun = (Get-Volume -FileSystemLabel 'OS').DriveLetter + ':\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\Add-Junctions.cmd'
-
-'chcp 65001 && echo off' | Out-File -Encoding unicode -FilePath $AutoRun
-'powershell.exe -encodedCommand {0}' -f $encodedCommand | Out-File -Encoding unicode -FilePath $AutoRun -Append
