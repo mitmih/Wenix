f30a6266b0c67507fd32ea8ea1d8ca76e78a0677
mitmih <38176228+mitmih@users.noreply.github.com>
2019-09-22 16:45:34 +0300

    Update Wenix-Config.psm1

--- Wenix/Wenix-Config.psm1
+++ Wenix/Wenix-Config.psm1
@@ -1,9 +1,9 @@
 $volumes = [ordered] @{  # схема разбивки ЖД
     # Active (bootmgr) + winPE RAM-disk + wim-files storage
-    'VolPE' = New-Object psobject -Property @{ letter = [char]'B' ; size = 25GB ; active = $true  ; label = 'PE' }
+    'VolPE' = New-Object psobject -Property @{ letter = [char]'B' ; size = 20GB ; active = $true  ; label = 'PE' }
     
     # windows volume
-    'VolOS' = New-Object psobject -Property @{ letter = [char]'O' ; size = 75GB ; active = $false ; label = 'OS' }
+    'VolOS' = New-Object psobject -Property @{ letter = [char]'O' ; size = 80GB ; active = $false ; label = 'OS' }
     
     # user data, will be resized to max
     'VolUD' = New-Object psobject -Property @{ letter = [char]'Q' ; size = 0    ; active = $false ; label = 'Data' }
