76a82c02a4165e1b7ca68d2447c2da57ef60e643
mitmih <38176228+mitmih@users.noreply.github.com>
2019-09-26 22:49:14 +0300

    Update Wenix-Config.psm1

--- Wenix/Wenix-Config.psm1
+++ Wenix/Wenix-Config.psm1
@@ -1,6 +1,6 @@
 $volumes = [ordered] @{  # схема разбивки ЖД
     # Active (bootmgr) + winPE RAM-disk + wim-files storage
-    'VolPE' = New-Object psobject -Property @{ letter = [char]'B' ; size = 20GB ; active = $true  ; label = 'PE' }
+    'VolPE' = New-Object psobject -Property @{ letter = [char]'B' ; size = 25GB ; active = $true  ; label = 'PE' }
     
     # windows volume
     'VolOS' = New-Object psobject -Property @{ letter = [char]'O' ; size = 80GB ; active = $false ; label = 'OS' }
@@ -9,7 +9,7 @@ $volumes = [ordered] @{  # схема разбивки ЖД
     'VolUD' = New-Object psobject -Property @{ letter = [char]'Q' ; size = 0    ; active = $false ; label = 'Data' }
 }
 
-
+# TODO: в случае нескольких дисков нужно показывать инфу по дискам и давать пользователю выбор индекса целевого диска, наподобие Show-Menu
 $DiskNumber = 0  # работа всегда идёт с первым диском в системе
 
 
