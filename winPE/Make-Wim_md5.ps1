7fde4b52f94048f181a4d84826eb7200f7e05dfb
mitmih <38176228+mitmih@users.noreply.github.com>
2019-11-03 22:05:56 +0300

    Update Make-Wim_md5.ps1
    теперь, вместо жёстко заданного .\.pe_work_dir\amd64\media\sources\boot.wim
    подсчёт md5-хэша выполняется для любого файла

--- winPE/Make-Wim_md5.ps1
+++ winPE/Make-Wim_md5.ps1
@@ -1,5 +1,16 @@
+param (
+    [string] $path = ".\.pe_work_dir\amd64\media\sources\boot.wim"
+    )
+
 Set-Location "$($MyInvocation.MyCommand.Definition | Split-Path -Parent)"
 
-$HashSum = Get-FileHash -Algorithm MD5 -Path ".\.pe_work_dir\amd64\media\sources\boot.wim"
+# $HashSum = Get-FileHash -Algorithm MD5 -Path ".\.pe_work_dir\amd64\media\sources\boot.wim"
 
-$HashSum.Hash + ' *boot.wim' | Out-File -Encoding ascii -FilePath ".\.pe_work_dir\amd64\media\sources\boot.wim.md5"
+# $HashSum.Hash + ' *boot.wim' | Out-File -Encoding ascii -FilePath ".\.pe_work_dir\amd64\media\sources\boot.wim.md5"
+
+if (Test-Path -Path $path)
+{
+    $HashSum = Get-FileHash -Algorithm MD5 -Path $path
+    
+    $HashSum.Hash + ' *' + $path.Split('\')[-1] | Out-File -Encoding ascii -FilePath ($path + ".md5")
+}
