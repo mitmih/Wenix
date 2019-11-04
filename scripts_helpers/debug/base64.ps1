7b255bb0b605c191570b6c1ce37a3658889b1ba5
mitmih <38176228+mitmih@users.noreply.github.com>
2019-11-04 23:30:00 +0300

    пример работы с кодировками
    base64
    utf8 with BOM
    utf8 wthout BOM

--- scripts_helpers/debug/base64.ps1
+++ scripts_helpers/debug/base64.ps1
@@ -1,14 +1,28 @@
-#     кодируем аргументы в base64-строку
-#     $command = "$env:SystemDrive\Capture-Wim.ps1"
-#     $bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
-#     $encodedCommand = [Convert]::ToBase64String($bytes)
+Clear-Host
 
-$txt_open = "администратор и его пароль"
-$txt_bytesEnc = [System.Text.Encoding]::Unicode.GetBytes($txt_open)
-$txt_encoded = [System.Convert]::ToBase64String($txt_bytesEnc)
+$current_dir = $MyInvocation.MyCommand.Definition | Split-Path -Parent
+Set-Location $current_dir
 
-$txt_bytesDec = [System.Convert]::FromBase64String($txt_encoded)
-$txt_open_decoded = [System.Text.Encoding]::Unicode.GetString($txt_bytesDec)
+$UTF8BOM__no = New-Object System.Text.UTF8Encoding $false
+$UTF8BOM_yes = New-Object System.Text.UTF8Encoding $true
 
-$txt_open
-$txt_open_decoded
+$file = Import-Csv -Path '.\base64.csv'
+
+foreach ($s in $file)
+{
+    try   { $s.user = $UTF8BOM__no.GetString([System.Convert]::FromBase64String($s.user    )) }  # если ещё не в base64
+    catch { $s.user = [System.Convert]::ToBase64String($UTF8BOM__no.GetBytes($s.user)) }         # то закодируем
+    
+    try   { $s.password = $UTF8BOM__no.GetString([System.Convert]::FromBase64String($s.password)) }
+    catch { $s.password = [System.Convert]::ToBase64String($UTF8BOM__no.GetBytes($s.password)) }
+    
+    Write-Host $s.user, $s.password
+}
+
+
+$file | Export-Csv -NoTypeInformation -Path '.\base64.inverted.csv' -Encoding UTF8
+sleep -Milliseconds 333
+
+# перекодировка utf8_BOM >>> utf8_no_BOM
+$text = [System.IO.File]::ReadAllLines('.\base64.inverted.csv', $UTF8BOM_yes)
+[System.IO.File]::WriteAllLines('.\base64.inverted.csv', $text, $UTF8BOM__no)
