1459f3c82504381e0779e41c96d49a64b4af74a5
mitmih <38176228+mitmih@users.noreply.github.com>
2019-12-10 23:22:33 +0300

    Update Wenix-Debug3.ps1

--- Wenix/Wenix-Debug3.ps1
+++ Wenix/Wenix-Debug3.ps1
@@ -1,21 +1,28 @@
 # https://docs.microsoft.com/ru-ru/dotnet/api/system.io.file.openread?view=netframework-4.8
 # https://docs.microsoft.com/ru-ru/dotnet/api/system.io.file.openwrite?view=netframework-4.8
 
-$file_read  = "C:\.it\PE\boot.wim.md5"
-$file_write = "C:\.it\PE\boot.wim.md5.NEW"
+$WatchDogTimer = [system.diagnostics.stopwatch]::startNew()
+
+$file_read  = "C:\.it\PE\boot.wim"
+$file_write = "C:\.it\PE\boot.wim_1"
 
 try
 {
     $source = [System.IO.File]::OpenRead(  $file_read  )
     $target = [System.IO.File]::OpenWrite( $file_write )
     
-    [byte[]] $buf = New-Object byte[] 16  # 1024
     
-    while ($source.read($buf, 0, $buf.Length) -gt 0)
+    [byte[]] $buf = New-Object byte[] 1023  # 1024
+    
+    [uint32] $tail = $source.Length % $buf.Length
+    
+    while ($source.Read($buf, 0, $buf.Length) -gt $tail)
     {
-        [System.Text.Encoding]::Default.GetString($buf)
-        $target.write($buf, 0, $buf.Length)
+        # $source.Length, $source.Position, [System.Text.Encoding]::Default.GetString($buf)
+        $target.Write($buf, 0, $buf.Length)
     }
+    
+    if ($tail -gt 0) { $target.Write($buf[0..($tail - 1)], 0, $tail) }
 }
 
 finally
@@ -23,3 +30,5 @@ finally
     $source.Dispose()
     $target.Dispose()
 }
+
+Write-Host ("{0,5:N2} minutes" -f $WatchDogTimer.Elapsed.TotalMinutes)
