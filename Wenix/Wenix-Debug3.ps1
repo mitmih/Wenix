913e22c4a7eee5d02482cc1fc367a9ad20b98663
mitmih <38176228+mitmih@users.noreply.github.com>
2019-12-10 23:43:00 +0300

    Update Wenix-Debug3.ps1
    в целом копирование работает на ~400 МБ файле, md5-хэши исходного и нового файла совпадают после копирования
    
    далее нужно "прикрутить" шкалу прогресса
    + скроее всего в процессе прикручивание получиться избавиться от $tail, т.е. получиться в цикле читать не весь буфер, а необходимый кусок, хотя возможно вычисление длины чтения для каждой итерации скажется на производительности чтения

--- Wenix/Wenix-Debug3.ps1
+++ Wenix/Wenix-Debug3.ps1
@@ -3,32 +3,36 @@
 
 $WatchDogTimer = [system.diagnostics.stopwatch]::startNew()
 
-$file_read  = "C:\.it\PE\boot.wim"
-$file_write = "C:\.it\PE\boot.wim_1"
+$file_read  = "C:\.it\PE\boot.wim"    # исходник
+$file_write = "C:\.it\PE\boot.wim_1"  # новый файл
 
 try
 {
-    $source = [System.IO.File]::OpenRead(  $file_read  )
-    $target = [System.IO.File]::OpenWrite( $file_write )
+    $source = [System.IO.File]::OpenRead(  $file_read  )  # исходник
+    
+    $target = [System.IO.File]::OpenWrite( $file_write )  # новый файл
     
     
-    [byte[]] $buf = New-Object byte[] 1023  # 1024
+    [byte[]] $buf = New-Object byte[] 1023  # буфер чтения
     
-    [uint32] $tail = $source.Length % $buf.Length
+    [uint32] $tail = $source.Length % $buf.Length  # остаток от деленеия ~ размер хвоста, который нужно будет дописать после цикла
     
-    while ($source.Read($buf, 0, $buf.Length) -gt $tail)
-    {
-        # $source.Length, $source.Position, [System.Text.Encoding]::Default.GetString($buf)
-        $target.Write($buf, 0, $buf.Length)
-    }
+    while ($source.Read($buf, 0, $buf.Length) -gt $tail) { $target.Write($buf, 0, $buf.Length) }
     
-    if ($tail -gt 0) { $target.Write($buf[0..($tail - 1)], 0, $tail) }
+    if ($tail -gt 0) { $target.Write($buf[ 0..($tail - 1) ], 0, $tail) }  # запись хвостика
 }
 
 finally
 {
     $source.Dispose()
+    
     $target.Dispose()
 }
+
+Write-Host ("{0,5:N2} minutes" -f $WatchDogTimer.Elapsed.TotalMinutes)
+
+$HashSum = Get-FileHash -Algorithm MD5 -Path $file_write
+
+$HashSum.Hash + ' *' + $file_write.Split('\')[-1] | Out-File -Encoding ascii -FilePath ($file_write + ".md5")
 
 Write-Host ("{0,5:N2} minutes" -f $WatchDogTimer.Elapsed.TotalMinutes)
